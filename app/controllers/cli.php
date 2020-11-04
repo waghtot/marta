00b096eef4e5490709143fb9a72c9b7baa7262bc
waghtot <waghtot@gmail.com>
2020-11-04 19:46:02 +0000

    first draft

---
+++ app/controllers/cli.php
@@ -0,0 +1,48 @@
+<?php
+
+class Cli
+{
+
+	$data = array();
+
+	if(isset($argv))
+	{
+		checkRrgv($argv); 
+	}else{
+		response();
+	}
+
+	function checkRrgv($input)
+	{
+		if(isset($input[1]))
+		{
+			 echo heniek($input[1]);
+		}else{
+			response();
+		}
+	}
+
+	function heniek($input)
+	{ 
+		return color($input);	
+	}
+
+	function color($input)
+	{
+		switch($input){
+			case 'rozowy':
+				return 'dobrze';
+			break;
+			default:
+				return 'nie dobrze';
+			break;
+		}
+
+	}
+
+	function response()
+	{
+		echo 'brak danych wejsciowych';
+		exit;
+	}
+}
